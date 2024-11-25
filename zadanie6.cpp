9aa01b9b2bb0cbe692e65035a32917f8dac6b489
Vitaliy-Wyzer <105667219+Vitaliy-Wyzer@users.noreply.github.com>
2024-11-25 17:29:29 +0100

    Update zadanie6.cpp

--- zadanie6.cpp
+++ zadanie6.cpp
@@ -1,96 +1,100 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
-#include <string>
-#include <sstream>
 
-struct Node {
-    int id;
-    float x, y;
+using namespace std;
+
+class Point {
+public:
+  float x,y;
 };
 
-struct Polygon {
-    int* nodeIds;
-    int size;
+class Wielokat {
+public:
+  Point * p;
+  int * ids;
+  int size;
 };
 
 float distance(float x1, float y1, float x2, float y2) {
-    return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+  return sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
 }
 
-float Area(const Node* nodes, const Polygon& polygon) {
-    float ar = 0.0f;
-    for (int i = 0; i < polygon.size; ++i) { 
-        const Node& current = nodes[polygon.nodeIds[i] - 1];
-        const Node& next = nodes[polygon.nodeIds[(i + 1) % polygon.size] - 1];
-        ar += current.x * next.y - next.x * current.y;
-    }
-    return std::abs(ar) / 2.0f;
+float Pole(const Point * p, const Wielokat& wielokat) {
+  float pole = 0.0f;
+  for (int i = 0; i<wielokat.size; ++i) {
+    const Point& current = p[wielokat.ids[i] - 1];
+    const Point& next = p[wielokat.ids[(i+1) % wielokat.size] - 1];
+    pole += current.x * next.y - next.x * current.y;
+  }
+  return abs(pole) / 2.0f;
 }
 
-float Perimeter(const Node* nodes, const Polygon& polygon) {
-    float p = 0.0f;
-    for (int i = 0; i < polygon.size; ++i) { 
-        const Node& current = nodes[polygon.nodeIds[i] - 1];
-        const Node& next = nodes[polygon.nodeIds[(i + 1) % polygon.size] - 1];
-        p += distance(current.x, current.y, next.x, next.y); 
-    }
-    return p;
+float Obwod(const Point * p, const Wielokat& wielokat) {
+  float obwod = 0.0f;
+  for (int i = 0; i<wielokat.size; ++i) {
+    const Point& current = p[wielokat.ids[i] - 1];
+    const Point& next = p[wielokat.ids[(i+1) % wielokat.size] - 1];
+    obwod += distance(current.x, current.y, next.x, next.y);
+  }
+  return fabs(obwod);
 }
+
+// ids table
+// getline
+// >>
+// while do konca pliku
+// getline sprawdzenie
+// f >> id >> x >> y; n razy
+
+
+
 
 int main() {
-    std::ifstream inputFile("ok.txt");
-    if (!inputFile.is_open()) {
-        std::cerr << "Error: Could not open file!" << std::endl;
-        return 1;
+  ifstream fin("ok.txt");
+  if (!fin.good()) {
+    cerr << "Blad pliku";
+    fin.clear();
+    fin.ignore();
+    fin.close();
+    return -1;
+  }
+
+  Wielokat wielokat;
+  wielokat.p = nullptr;
+  wielokat.ids = nullptr;
+  wielokat.size = 0;
+
+  string line;
+  while (getline(fin, line)) {
+    if (line.find("[NUMBER OF NODES]") != string::npos) {
+      fin >> wielokat.size;
+      wielokat.p = new Point[wielokat.size];
+      getline(fin, line);
+    } else if (line.find("[NODES]") != string::npos) {
+      for (int i = 0; i<wielokat.size; ++i) {
+        int id;
+        fin >> id;
+        fin >> wielokat.p[id-1].x >> wielokat.p[id-1].y;
+      }
+    } else if (line.find("[POLYGON]") != string::npos) {
+      wielokat.ids = new int[wielokat.size];
+      for (int i = 0; i<wielokat.size; ++i) {
+        fin >> wielokat.ids[i];
+      }
     }
+  }
+  fin.clear();
+  fin.close();
 
-    std::string line;
-    int numberOfNodes = 0;
-    Node* nodes = nullptr;
-    Polygon polygon;
+  float obwod = Obwod(wielokat.p, wielokat);
+  float pole = Pole(wielokat.p, wielokat);
 
-    polygon.nodeIds = nullptr;
-    polygon.size = 0;
+  cout << "Obwod: " << obwod << endl;
+  cout << "Pole: " << pole << endl;
 
-    while (std::getline(inputFile, line)) {
-        if (line.find("[NUMBER OF NODES]") != std::string::npos) {
-            inputFile >> numberOfNodes;
-            nodes = new Node[numberOfNodes];
-            std::getline(inputFile, line);
-        } else if (line.find("[NODES]") != std::string::npos) {
-            for (int i = 0; i < numberOfNodes; ++i) {
-                inputFile >> nodes[i].id >> nodes[i].x >> nodes[i].y;
-            }
-        } else if (line.find("[POLYGON]") != std::string::npos) {
-            std::getline(inputFile, line);
-            std::istringstream polygonStream(line);
-            int tempNodeCount = 0;
+  delete [] wielokat.p;
+  delete [] wielokat.ids;
 
-            while (polygonStream >> tempNodeCount) {
-                ++polygon.size;
-            }
-
-            polygon.nodeIds = new int[polygon.size];
-
-            polygonStream.clear();
-            polygonStream.seekg(0, std::ios::beg);
-            for (int i = 0; i < polygon.size; ++i) {
-                polygonStream >> polygon.nodeIds[i];
-            }
-        }
-    }
-
-    inputFile.close();
-
-    float perimeter = Perimeter(nodes, polygon);
-    float area = Area(nodes, polygon);
-
-    std::cout << "Perimeter: " << perimeter << std::endl;
-    std::cout << "Area: " << area << std::endl;
-
-    delete[] nodes;
-    delete[] polygon.nodeIds;
-
-    return 0;
+  return 0;
 }
