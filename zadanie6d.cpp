b93eba592684955553d5ee19cd22454c41ed6e28
Vitaliy-Wyzer <105667219+Vitaliy-Wyzer@users.noreply.github.com>
2024-12-29 14:21:49 +0100

    Update zadanie6d.cpp

--- zadanie6d.cpp
+++ zadanie6d.cpp
@@ -2,119 +2,109 @@
 #include <cmath>
 #include <fstream>
 #include <stdexcept>
-#include <string>
 
 using namespace std;
 
+class InvalidFileStructure : public runtime_error {
+public:
+  InvalidFileStructure(const string& message) : runtime_error(message) {}
+};
+
 class Point {
 public:
-    float x, y;
+  float x, y;
 };
 
 class Wielokat {
 public:
-    Point* p;
-    int* ids;
-    int size;
+  Point * p;
+  int * ids;
+  int size;
 
-    Wielokat() : p(nullptr), ids(nullptr), size(0) {}
+  Wielokat() : p(nullptr), ids(nullptr), size(0) {}
 
-    ~Wielokat() {
-        delete[] p;
-        delete[] ids;
+  void wczytajZPliku(const string& nazwaPliku) {
+    ifstream fin(nazwaPliku);
+    if (!fin.good()) {
+      throw InvalidFileStructure("Blad pliku: Nie udalo sie otworzyc pliku.");
     }
 
-    void wczytajZPliku(const string& nazwaPliku) {
-        ifstream fin(nazwaPliku);
-        if (!fin.is_open()) {
-            throw runtime_error("Nie można otworzyć pliku: " + nazwaPliku);
+    string line;
+    while (getline(fin, line)) {
+      if (line.find("[NUMBER OF NODES]") != string::npos) {
+        if (!(fin >> size) || size <= 0) {
+          throw InvalidFileStructure("Blad pliku: Nieprawidlowy rozmiar w [NUMBER OF NODES].");
         }
-
-        string line;
-        bool numberOfNodesRead = false;
-        bool nodesRead = false;
-        bool polygonRead = false;
-
-        while (getline(fin, line)) {
-            if (line.find("[NUMBER OF NODES]") != string::npos) {
-                fin >> size;
-                if (size <= 0) {
-                    throw runtime_error("Nieprawidłowa liczba węzłów w pliku.");
-                }
-                p = new Point[size];
-                getline(fin, line); // Przejdź do następnej linii
-                numberOfNodesRead = true;
-            } else if (line.find("[NODES]") != string::npos) {
-                if (!numberOfNodesRead) {
-                    throw runtime_error("Sekcja [NUMBER OF NODES] musi być przed sekcją [NODES].");
-                }
-                for (int i = 0; i < size; ++i) {
-                    int id;
-                    if (!(fin >> id >> p[id - 1].x >> p[id - 1].y)) {
-                        throw runtime_error("Nieprawidłowe dane w sekcji [NODES].");
-                    }
-                }
-                nodesRead = true;
-            } else if (line.find("[POLYGON]") != string::npos) {
-                if (!nodesRead) {
-                    throw runtime_error("Sekcja [NODES] musi być przed sekcją [POLYGON].");
-                }
-                ids = new int[size];
-                for (int i = 0; i < size; ++i) {
-                    if (!(fin >> ids[i])) {
-                        throw runtime_error("Nieprawidłowe dane w sekcji [POLYGON].");
-                    }
-                }
-                polygonRead = true;
-            }
+        p = new Point[size];
+        getline(fin, line);
+      } else if (line.find("[NODES]") != string::npos) {
+        for (int i = 0; i < size; ++i) {
+          int id;
+          if (!(fin >> id) || id <= 0) {
+            throw InvalidFileStructure("Blad pliku: Nieprawidlowe ID punktu w [NODES].");
+          }
+          fin >> p[id - 1].x >> p[id - 1].y;
         }
-
-        if (!numberOfNodesRead || !nodesRead || !polygonRead) {
-            throw runtime_error("Brak wymaganych sekcji w pliku.");
+      } else if (line.find("[POLYGON]") != string::npos) {
+        if (ids) {
+          delete[] ids;
         }
-
-        fin.close();
+        ids = new int[size];
+        for (int i = 0; i < size; ++i) {
+          if (!(fin >> ids[i]) || ids[i] <= 0 || ids[i] > size) {
+            throw InvalidFileStructure("Blad pliku: Nieprawidlowe ID w [POLYGON].");
+          }
+        }
+      }
     }
+
+    fin.clear();
+    fin.close();
+  }
 };
 
 float distance(float x1, float y1, float x2, float y2) {
-    return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+  return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
 
-float Pole(const Point* p, const Wielokat& wielokat) {
-    float pole = 0.0f;
-    for (int i = 0; i < wielokat.size; ++i) {
-        const Point& current = p[wielokat.ids[i] - 1];
-        const Point& next = p[wielokat.ids[(i + 1) % wielokat.size] - 1];
-        pole += current.x * next.y - next.x * current.y;
-    }
-    return abs(pole) / 2.0f;
+float Pole(const Point * p, const Wielokat& wielokat) {
+  float pole = 0.0f;
+  for (int i = 0; i < wielokat.size; ++i) {
+    const Point& current = p[wielokat.ids[i] - 1];
+    const Point& next = p[wielokat.ids[(i + 1) % wielokat.size] - 1];
+    pole += current.x * next.y - next.x * current.y;
+  }
+  return abs(pole) / 2.0f;
 }
 
-float Obwod(const Point* p, const Wielokat& wielokat) {
-    float obwod = 0.0f;
-    for (int i = 0; i < wielokat.size; ++i) {
-        const Point& current = p[wielokat.ids[i] - 1];
-        const Point& next = p[wielokat.ids[(i + 1) % wielokat.size] - 1];
-        obwod += distance(current.x, current.y, next.x, next.y);
-    }
-    return fabs(obwod);
+float Obwod(const Point * p, const Wielokat& wielokat) {
+  float obwod = 0.0f;
+
+  for (int i = 0; i < wielokat.size; ++i) {
+    const Point& current = p[wielokat.ids[i] - 1];
+    const Point& next = p[wielokat.ids[(i + 1) % wielokat.size] - 1];
+    obwod += distance(current.x, current.y, next.x, next.y);
+  }
+  return fabs(obwod);
 }
 
 int main() {
-    try {
-        Wielokat wielokat;
-        wielokat.wczytajZPliku("bad.txt");
+  try {
+    Wielokat wielokat;
+    wielokat.wczytajZPliku("bad.txt");
 
-        float obwod = Obwod(wielokat.p, wielokat);
-        float pole = Pole(wielokat.p, wielokat);
+    float obwod = Obwod(wielokat.p, wielokat);
+    float pole = Pole(wielokat.p, wielokat);
 
-        cout << "Obwod: " << obwod << endl;
-        cout << "Pole: " << pole << endl;
-    } catch (const exception& e) {
-        cerr << "Wystąpił błąd: " << e.what() << endl;
-        return -1;
-    }
+    cout << "Obwod: " << obwod << endl;
+    cout << "Pole: " << pole << endl;
 
-    return 0;
+    delete[] wielokat.p;
+    delete[] wielokat.ids;
+  } catch (const InvalidFileStructure& e) {
+    cerr << "Wystapil blad: " << e.what() << endl;
+    return -1;
+  }
+
+  return 0;
 }
